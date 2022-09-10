a1033376564ec07d8a3ab7a200981f9b2d8384ad
Andrei <fmwar92@gmail.com>
2022-09-10 13:44:28 +0300

    worked with lists

--- home_work2/task_3.py
+++ home_work2/task_3.py
@@ -4,9 +4,11 @@ number_k = float(input('Введите число k: '))
 list = []
 i = 1 # левая граница значений 1 до k
 x = 1 # элементы последовательности (1 + 1\k)^k
+summ_list = 0
 
 while i <= number_k:
     x = round((1 + 1/i)**i, 2)
     list.append(x)
+    summ_list += x
     i += 1
-print(f'Cписок из k чисел последовательности (1 + 1\k)^k: \n{list}')
+print(f'Сумма элементов списка {list} из k чисел = {summ_list}')
